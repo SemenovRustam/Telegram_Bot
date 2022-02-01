5ebe5db856bf6ecf59b1f1260c978a060101ddc2
SemenovRustam <zeevvsss@yandex.ru>
2022-02-01 14:26:57 +0500

    step 5 add Test database add stat for telegram_bot

---
+++ src/test/java/com/rustam_semenov/telegramm_bot/demo/StatCommandTest.java
@@ -0,0 +1,24 @@
+package com.rustam_semenov.telegramm_bot.demo;
+
+import com.rustam_semenov.telegramm_bot.demo.command.Command;
+import com.rustam_semenov.telegramm_bot.demo.command.StatCommand;
+
+import static com.rustam_semenov.telegramm_bot.demo.command.CommandName.STAT;
+import static com.rustam_semenov.telegramm_bot.demo.command.StatCommand.STAT_MESSAGE;
+
+public class StatCommandTest extends AbstractCommandTest {
+    @Override
+    String getCommandName() {
+        return STAT.getCommandName();
+    }
+
+    @Override
+    String getCommandMessage() {
+        return String.format(STAT_MESSAGE, 0);
+    }
+
+    @Override
+    Command getCommand() {
+        return new StatCommand(sendBotMessageService, telegramUserService);
+    }
+}
